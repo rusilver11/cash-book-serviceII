066dfb463a9295a894fedb390c479a640b038851
rusilver11 <rusilver11@gmail.com>
2022-03-21 18:20:53 +0700

    delete pm2

--- ecosystem.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps : [{
-    name   : "cash-book-service",
-    script : "server.js",
-    watch: ["server"],
-    ignore_watch : ["wasession*.json", "wasessions.json", "test/**/*.js"],
-    watch : true
-  }]
-}
